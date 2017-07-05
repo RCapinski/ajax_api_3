ae20787ec17ffea381dcd86f24e3d4b5e9f7c6f7
RCapinski <rafalcapinski@gmail.com>
2017-07-05 20:37:23 +0200

    source files added

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(function() {
+	var url = 'https://restcountries.eu/rest/v1/name/';
+	var countriesList = $('#countries');
+	
+	$('#search').click(searchCountries);
+	
+	function searchCountries () {
+		var countryName = $('#country-name').val();
+		if (!countryName.length) countryName = 'Poland';
+		$.ajax({
+			url: url + countryName,
+			method: 'GET',
+			success: showCountriesList,
+      error: errorFunction
+		});
+	};
+  
+  function errorFunction () {
+    alert('No country found!');
+  };
+	
+	function showCountriesList (resp) {
+    if (!resp) {
+      alert('Countries info could not be loaded!');
+      return null;
+    };
+		countriesList.empty();
+		resp.forEach(function(item) {
+			$('<li>').text(item.name + ' - capital: ' + item.capital).appendTo(countriesList);
+		});
+	};
+})
